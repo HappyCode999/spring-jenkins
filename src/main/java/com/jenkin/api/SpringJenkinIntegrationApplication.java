e4e826cf90651969157e6113b0c5e4b541705066
codestart999 <happysah999@gmail.com>
2022-04-15 12:23:29 +0530

    third  commit

--- src/main/java/com/jenkin/api/SpringJenkinIntegrationApplication.java
+++ src/main/java/com/jenkin/api/SpringJenkinIntegrationApplication.java
@@ -17,7 +17,7 @@ public class SpringJenkinIntegrationApplication {
 	@PostConstruct
 	public void init() {
 		logger.info("App started");
-		logger.info("App started second time");
+		logger.info("App started thrid time");
 	}
 
 	public static void main(String[] args) {
