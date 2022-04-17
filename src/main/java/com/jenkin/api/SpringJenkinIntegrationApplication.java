61464f42c6b2b9b54acb9a98f818fb0729205a37
codestart999 <happysah999@gmail.com>
2022-04-17 13:32:44 +0530

    k8s deployment

--- src/main/java/com/jenkin/api/SpringJenkinIntegrationApplication.java
+++ src/main/java/com/jenkin/api/SpringJenkinIntegrationApplication.java
@@ -6,24 +6,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringJenkinIntegrationApplication {
-	
+
 	static Logger logger = LoggerFactory.getLogger(SpringJenkinIntegrationApplication.class);
-	
+
+	@RequestMapping("/message")
+	public String displayMessage() {
+		return "Congratulation, you have successfully deployed your application to k8s !!";
+	}
+
 	@PostConstruct
 	public void init() {
-		logger.info("App started final");
+		logger.info("App started");
 	}
 
 	public static void main(String[] args) {
 		logger.info("App executed!");
 		SpringApplication.run(SpringJenkinIntegrationApplication.class, args);
 	}
-	
-	
 
 }
